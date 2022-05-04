183a77a832c9b5c0e13442042ec9d2400df0f351
tu4k0 <91626915+tu4k0@users.noreply.github.com>
2022-05-04 08:34:21 +0300

    Supply_Chain_Project ver. 0.4
    Added events for payment checking and delivering item.

--- Supply_Chain/client/src/App.js
+++ Supply_Chain/client/src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
+      this.listenToPaymentEvent();
       this.setState({ loaded: true });
     } catch (error) {
       // Catch any errors for any of the above operations.
@@ -40,6 +41,15 @@ class App extends Component {
       console.error(error);
     }
   };
+
+  listenToPaymentEvent = () => {
+    let self = this;
+    this.itemManager.events.SupplyChainStep().on("data", async function(evt){
+      console.log(evt);
+      let itemObj = await self.itemManager.methods.items(evt.returnValues._itemIndex).call();
+      alert("Item " + itemObj._identifier + " was paid. Deliver it now");
+    })
+  }
 
   handleInputChange = (event) => {
     const target = event.target;
